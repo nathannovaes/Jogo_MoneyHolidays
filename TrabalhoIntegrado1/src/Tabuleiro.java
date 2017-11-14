4c97a10584e3f050f53560d1f5b2ef67f9eb3944
nvitiver <nathan.vitiver@gmail.com>
2017-11-14 15:06:29 -0200

    1.7
    Adicionar som de fundo

--- TrabalhoIntegrado1/src/Tabuleiro.java
+++ TrabalhoIntegrado1/src/Tabuleiro.java
@@ -1,7 +1,16 @@
+import java.io.File;
 import java.util.ArrayList;
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.DataLine;
+import javax.sound.sampled.SourceDataLine;
 import javax.swing.JOptionPane;
 
 public class Tabuleiro extends javax.swing.JFrame {
+        //SOM
+        Som somFundo = new Som();
+    
         //###############JOGADORES###############// -- COMEÇO     
         boolean presoVermelho = false;
         boolean presoAzul = false; 
@@ -528,13 +537,44 @@ public class Tabuleiro extends javax.swing.JFrame {
     }//GEN-LAST:event_boxCaixaAzulActionPerformed
 
     public static void main(String args[]) {
-        
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
                 new Tabuleiro().setVisible(true);
 
             }
         });
+        boolean music = true;
+      do{
+        SourceDataLine soundLine = null;
+        int BUFFER_SIZE = 64*1024;  // 64 KB
+
+        // Set up an audio input stream piped from the sound file.
+        try {
+           File soundFile = new File("C:\\Users\\Nathan\\Documents\\GitHub\\TrabalhoIntegrado\\TrabalhoIntegrado1\\src\\Musica\\Som");
+           AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(soundFile);
+           AudioFormat audioFormat = audioInputStream.getFormat();
+           DataLine.Info info = new DataLine.Info(SourceDataLine.class, audioFormat);
+           soundLine = (SourceDataLine) AudioSystem.getLine(info);
+           soundLine.open(audioFormat);
+           soundLine.start();
+           int nBytesRead = 0;
+           byte[] sampledData = new byte[BUFFER_SIZE];
+           while (nBytesRead != -1) {
+              nBytesRead = audioInputStream.read(sampledData, 0, sampledData.length);
+              if (nBytesRead >= 0) {
+                 // Writes audio data to the mixer via this source data line.
+                 soundLine.write(sampledData, 0, nBytesRead);
+              }
+           }
+        } catch (Exception e){
+            JOptionPane.showMessageDialog(null, "Erro no som!");
+        } finally {
+           soundLine.drain();
+           soundLine.close();
+        }          
+      }while(music == true);  
+        
+
         
     }
 
